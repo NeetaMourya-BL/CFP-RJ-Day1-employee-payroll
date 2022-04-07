29ffe620e1c3575fa58c757f550f39a227dbd524
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-04-07 20:28:09 +0530

    Added: component, router & payroll-form folder Create Payroll-form Page To add employee

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Router } from 'react-router-dom';
+import PayrollForm from './components/payrollform/PayrollForm';
+import TakeRouter from './router/router';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+     <TakeRouter/>
     </div>
   );
 }
-
 export default App;
