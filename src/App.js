52c21429957db34149719742386139b131f958d7
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-04-12 14:04:08 +0530

    Added: components & Assets folder for Create Payroll-form Page To add employee

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import logo from './logo.svg';
 import './App.css';
-import { Router } from 'react-router-dom';
-import PayrollForm from './components/payrollform/PayrollForm';
-import TakeRouter from './router/router';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
+import PayrollForm from './components/payroll-form/PayrollForm';
+import NotFound from './components/payroll-form/NotFound';
 
 function App() {
   return (
+    
+    <Router>
     <div className="App">
-     <TakeRouter/>
+
+      <Switch>
+        <Route exact path="/" component={PayrollForm} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
+  </Router>
+    
   );
 }
+
 export default App;
